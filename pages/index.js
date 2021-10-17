34f7496c6c54c24d37ebb5e4099eb21910611f41
XOP <wdybih@gmail.com>
2021-10-17 12:52:36 +0200

    init

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Heading from 'choom/lib/components/heading/Heading';
+
+import theme from 'choom/lib/theme/theme.css';
+
+export default function Intro () {
+  return (
+    <Heading level='2' as='h1' colorInherit>Your honest game</Heading>
+  )
+}
